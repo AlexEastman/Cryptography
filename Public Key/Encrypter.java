2d4b1fb7f81a24199fb4dd590b7eb142b9f86976
Alex Eastman <a-dog98@hotmail.com>
2015-04-13 12:16:19 -0500

    added key generator class
    generates a key and also added some docs to encrypter class

--- Public Key/Encrypter.java
+++ Public Key/Encrypter.java
@@ -2,22 +2,24 @@
 public abstract class Encrypter
 {
     
-    private int key;
+    private int key; //public key
     
-    public Encrypter(int publicKey)
+    public Encrypter(int publicKey) // sets the public key of the encrypter
     {
         key = publicKey;
     }
     
-    public abstract String EncryptString(String message);
+    public abstract String EncryptString(String message); //encrypts a string
        
-    public abstract int EncryptNumber(int message);
+    public abstract int EncryptNumber(int message); //encrypts an int
     
     public abstract int getPrivateKey();//determines private key value
    
-    public abstract String DecryptString(String message);
+    public abstract String DecryptString(String message); 
+    //calculates private key then decrypts
        
-    public abstract String DecryptString(String message, int privateKey);
+    public abstract String DecryptString(String message, int privateKey); 
+    //decrypts based off given private key 
       
     public abstract int DecryptNumber(int message);
    
